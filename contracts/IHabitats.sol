cc503f2dc7c5dffa733ee4edc22e3fb1b78f3b17
nicetech-creator <toptech20190609@gmail.com>
2021-10-19 23:12:45 +0800

    Meet token contract

---
+++ contracts/IHabitats.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.8.4;
+
+interface IHabitats {
+	function balanceOG(address _user) external view returns(uint256);
+}
---
+++ contracts/Meet.sol
@@ -0,0 +1,86 @@
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./IHabitats.sol";
+
+contract MeetToken is ERC20("Meet", "MEET") {
+	using SafeMath for uint256;
+
+	uint256 constant public BASE_RATE = 10 ether; 
+	uint256 constant public INITIAL_ISSUANCE = 300 ether;
+	// Tue Mar 18 2031 17:46:47 GMT+0000
+	uint256 constant public END = 1931622407;
+
+	mapping(address => uint256) public rewards;
+	mapping(address => uint256) public lastUpdate;
+
+	IHabitats public  habitatsContract;
+
+	event RewardPaid(address indexed user, uint256 reward);
+
+	constructor(address _habitats) public{
+		habitatsContract = IHabitats(_habitats);
+	}
+
+
+	function min(uint256 a, uint256 b) internal pure returns (uint256) {
+		return a < b ? a : b;
+	}
+
+	// called when minting many NFTs
+	// updated_amount = (balanceOG(user) * base_rate * delta / 86400) + amount * initial rate
+	function updateRewardOnMint(address _user, uint256 _amount) external {
+		require(msg.sender == address(habitatsContract), "Can't call this");
+		uint256 time = min(block.timestamp, END);
+		uint256 timerUser = lastUpdate[_user];
+		if (timerUser > 0)
+			rewards[_user] = rewards[_user].add(habitatsContract.balanceOG(_user).mul(BASE_RATE.mul((time.sub(timerUser)))).div(86400)
+				.add(_amount.mul(INITIAL_ISSUANCE)));
+		else 
+			rewards[_user] = rewards[_user].add(_amount.mul(INITIAL_ISSUANCE));
+		lastUpdate[_user] = time;
+	}
+
+	// called on transfers
+	function updateReward(address _from, address _to, uint256 _tokenId) external {
+		require(msg.sender == address(habitatsContract));
+		if (_tokenId < 1001) {
+			uint256 time = min(block.timestamp, END);
+			uint256 timerFrom = lastUpdate[_from];
+			if (timerFrom > 0)
+				rewards[_from] += habitatsContract.balanceOG(_from).mul(BASE_RATE.mul((time.sub(timerFrom)))).div(86400);
+			if (timerFrom != END)
+				lastUpdate[_from] = time;
+			if (_to != address(0)) {
+				uint256 timerTo = lastUpdate[_to];
+				if (timerTo > 0)
+					rewards[_to] += habitatsContract.balanceOG(_to).mul(BASE_RATE.mul((time.sub(timerTo)))).div(86400);
+				if (timerTo != END)
+					lastUpdate[_to] = time;
+			}
+		}
+	}
+
+	function getReward(address _to) external {
+		require(msg.sender == address(habitatsContract));
+		uint256 reward = rewards[_to];
+		if (reward > 0) {
+			rewards[_to] = 0;
+			_mint(_to, reward);
+			emit RewardPaid(_to, reward);
+		}
+	}
+
+	function burn(address _from, uint256 _amount) external {
+		require(msg.sender == address(habitatsContract));
+		_burn(_from, _amount);
+	}
+
+	function getTotalClaimable(address _user) external view returns(uint256) {
+		uint256 time = min(block.timestamp, END);
+		uint256 pending = habitatsContract.balanceOG(_user).mul(BASE_RATE.mul((time.sub(lastUpdate[_user])))).div(86400);
+		return rewards[_user] + pending;
+	}
+}
